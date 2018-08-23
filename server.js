722bf969329aa6b57b9f3801727ddc586ebbcb91
KevinWJohnson <kevin.johnson3@cox.net>
2018-08-23 14:20:53 -0700

    Update 8/23/2018 2:20pm

--- server.js
+++ server.js
@@ -74,6 +74,15 @@ app.get("/api/proxy/beaches", (req, res)=>{
     });
 })
 
+app.get("/api/proxy/forecast/:spot_id", (req, res)=>{
+  axios.get("http://api.spitcast.com/api/spot/forecast/" + req.params.spot_id)
+    .then(responseData => {
+      res.json(responseData.data);
+    }).catch(err => {
+      res.status(400).json(err);
+    });
+})
+
 
 // Add routes, both API and view
 app.use(routes);
